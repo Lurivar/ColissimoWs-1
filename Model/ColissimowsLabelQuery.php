5733a71bd93417c2b34e8998dd5d1cb524137d76
Lurivar <lurivar@users.noreply.github.com>
2020-04-03 14:21:01 +0200

    models

---
+++ Model/ColissimowsLabelQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ColissimoHomeDelivery\Model;
+
+use ColissimoHomeDelivery\Model\Base\ColissimowsLabelQuery as BaseColissimowsLabelQuery;
+
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'colissimows_label' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class ColissimowsLabelQuery extends BaseColissimowsLabelQuery
+{
+
+} // ColissimowsLabelQuery
